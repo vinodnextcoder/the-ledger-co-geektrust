7088d440e6cfa740b82b180c3f926ac1a656b13a
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2022-06-22 13:09:32 +0530

    command line input

--- geektrust.js
+++ geektrust.js
@@ -1,10 +1,40 @@
-const fs = require("fs")
+/**
+ * todo code is incomplete
+ */
+const fs = require("fs");
+const filename = process.argv[2];
 
-const filename = process.argv[2]
+function main(dataInput) {
+    var inputLines = dataInput.toString().split("\n")
+    for (i = 0; i < inputLines.length; i++) {
+        if (inputLines) {
+            let input = inputLines[i].split(' ')
+            switch (input[0]) {
+                case 'LOAN':
+                    loan(input[1], input[2], input[3], input[4], input[4])
+                    break;
+                case 'PAYMENT':
+                    payment(input[1], input[2], input[3], input[4]);
+                    break;
+                case 'BALANCE':
+                    balance(input[1], input[2], input[3]);
+                    break;
+            }
+        }
+    }
 
-fs.readFile(filename, "utf8", (err, data) => {
-    /*if (err) throw err
-    var inputLines = data.toString().split("\n")
-    // Add your code here to process input commands
-    */
-})
+}
+
+const loan = (bankName, borrowerName, Amount, years, interest) => {
+
+}
+const payment = (bankName, borrowerName, Amount, emiNo) => {
+
+}
+const balance = (bankName, borrowerName, emiNo) => {
+
+}
+
+data = fs.readFileSync(process.argv[2]).toString();
+main(data);
+module.exports = { main }
